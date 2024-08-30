6712def6439a95de4442da2566930e457154e2bb
adrianapereira27 <pereira.adri@gmail.com>
2024-08-30 16:57:34 -0300

    Validators

---
+++ CleanArch.Application/Validators/CursoUseCaseValidator.cs
@@ -0,0 +1,38 @@
+﻿using CleanArch.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Validators
+{
+    public class CursoUseCaseValidator : AbstractValidator<Curso>
+    {
+        public CursoUseCaseValidator()
+        {
+            RuleFor(a => a.Nome)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Obrigatório informar o Nome");
+
+            RuleFor(a => a.Nome)
+                .MaximumLength(100)
+                .WithMessage("Tamanho máximo do Nome é de 100 caracteres");
+
+            RuleFor(a => a.Descricao)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Obrigatório informar a Descrição");
+
+            RuleFor(a => a.Descricao)
+                .MaximumLength(500)
+                .WithMessage("Tamanho máximo da Descrição é de 500 caracteres");
+
+            RuleFor(a => a.IdProfessor)
+                .NotEqual(0)
+                .WithMessage("Obrigatório informar professor");
+        }
+    }
+}
---
+++ CleanArch.Application/Validators/ProfessorUseCaseValidator.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Domain.Entities;
+using FluentValidation;
+
+namespace CleanArch.Application.Validators
+{
+    public class ProfessorUseCaseValidator : AbstractValidator<Professor>
+    {
+        public ProfessorUseCaseValidator()
+        {
+            RuleFor(a => a.Nome)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Obrigatório informar o Nome");
+
+            RuleFor(a => a.Nome)
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo do Nome é de 150 caracteres");
+
+            RuleFor(a => a.Email)
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo do E-mail é de 150 caracteres");
+
+            RuleFor(a => a.Email)
+                .EmailAddress()
+                .WithMessage("E-mail não válido");
+
+        }
+    }
+}
